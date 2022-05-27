68f6b18974811f8ba7b7972edd149e22bec558d6
51r <petar.spasov@hashicorp.com>
2022-05-27 13:35:43 +0300

    added space between count and provisioner in the code

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "null" {}
 
 resource "null_resource" "text" {
   count = 3
+
   provisioner "local-exec" {
     command = "echo This is a sample use of null_resource provider with count meta-argument ${count.index + 1}"
   }
